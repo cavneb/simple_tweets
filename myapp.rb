d64cd904ca90947a51a9a4db8352d7ab23ba2be1
Eric Berry <eberry@tricky-mac.1on1.com>
2012-10-02 16:12:33 -0600

    finished project

--- myapp.rb
+++ myapp.rb
@@ -1,6 +1,23 @@
 require 'sinatra'
+require 'twitter'
 
 get '/' do
+
+  @styles = ['superhero', 'cerulean', 'cyborg', 'amelia', 'slate']
+  
+  # Assign the parameter value of `query` to the instance variable `@query`
+  @query = params['query']
+
+  # Set @tweets to default to an empty array
+  @tweets = []
+
+  # Only search Twitter if the query has a value
+  if @query && @query.length > 0
+    client = Twitter::Client.new
+    tweet_container = client.search(@query)
+    @tweets = tweet_container.results
+  end
+
   erb :index
 end
 
